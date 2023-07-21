e74ad9a5156a826eb201a0c5c2d54998c52d68ff
nbgraham <nickbgraham83@gmail.com>
2023-07-21 13:23:04 -0500

    feat: edit env variables

--- packages/insomnia/src/ui/components/templating/variable-editor.tsx
+++ packages/insomnia/src/ui/components/templating/variable-editor.tsx
@@ -1,6 +1,10 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useEffect, useMemo, useReducer, useRef, useState } from 'react';
+import { useFetcher, useParams, useRouteLoaderData } from 'react-router-dom';
 
+import type { Environment } from '../../../models/environment';
 import { useNunjucks } from '../../context/nunjucks/use-nunjucks';
+import { WorkspaceLoaderData } from '../../routes/workspace';
+import { createKeybindingsHandler } from '../keydown-binder';
 
 interface Props {
   defaultValue: string;
@@ -14,6 +18,29 @@ export const VariableEditor: FC<Props> = ({ onChange, defaultValue }) => {
   const [preview, setPreview] = useState('');
   const [error, setError] = useState('');
 
+  const { organizationId, projectId, workspaceId } = useParams<{ organizationId: string; projectId: string; workspaceId: string }>();
+  const updateEnvironmentFetcher = useFetcher();
+  const updateEnvironment = async (environmentId: string, patch: Partial<Environment>) => {
+    updateEnvironmentFetcher.submit({
+      patch,
+      environmentId,
+    },
+    {
+      encType: 'application/json',
+      method: 'post',
+      action: `/organization/${organizationId}/project/${projectId}/workspace/${workspaceId}/environment/update`,
+    });
+  };
+
+  const routeData = useRouteLoaderData(
+    ':workspaceId'
+  ) as WorkspaceLoaderData;
+
+  const {
+    activeEnvironment,
+  } = routeData;
+
+  const [edits, incrementEdits] = useReducer((state: number) => state + 1, 0);
   useEffect(() => {
     let isMounted = true;
     const syncInterpolation = async () => {
@@ -32,9 +59,54 @@ export const VariableEditor: FC<Props> = ({ onChange, defaultValue }) => {
     return () => {
       isMounted = false;
     };
-  }, [handleGetRenderContext, handleRender, selected]);
+  }, [handleGetRenderContext, handleRender, selected, edits]);
 
   const isCustomTemplateSelected = !options.find(v => selected === `{{ ${v.name} }}`);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const updateKey = useMemo(() => {
+    if (isCustomTemplateSelected) {
+      return null;
+    }
+    const withoutBraces = selected.substring(3, selected.length - 3);
+    if (!withoutBraces.startsWith('_.')) {
+      console.warn(`Key must start with '_.': ${withoutBraces}`);
+      return;
+    }
+    const withoutPrefix = withoutBraces.substring(2);
+    const key = withoutPrefix.split('.');
+    if (key.length === 0) {
+      return null;
+    }
+    return key;
+  }, [isCustomTemplateSelected, selected]);
+
+  const updateVariable = async (key: string[], newValue: string) => {
+    if (newValue === preview) {
+      return;
+    }
+
+    const keyCopy = [...key];
+    const first = keyCopy.pop();
+    if (!first) {
+      return;
+    }
+    let data: Record<string, any> = { [first]: newValue };
+    while (keyCopy.length > 0) {
+      const key = keyCopy.pop();
+      if (!key) {
+        break;
+      }
+      data = { [key]: data };
+    }
+
+    // TODO: This is _replacing_ the environment, removing all existing values
+    await updateEnvironment(activeEnvironment._id, {
+      data: data,
+    });
+  };
+
   return (
     <div>
       <div className="form-control form-control--outlined">
@@ -74,6 +146,30 @@ export const VariableEditor: FC<Props> = ({ onChange, defaultValue }) => {
           <textarea className={`${error ? 'danger' : ''}`} value={preview || error} readOnly />
         </label>
       </div>
+      {updateKey && (
+        <div className="form-control form-control--outlined">
+          <label>
+            Update value (Enter to save)
+            <input
+              ref={inputRef}
+              type="text"
+              title="Update value"
+              onKeyDown={createKeybindingsHandler({
+                'Enter': e => {
+                  e.stopPropagation();
+                  e.preventDefault();
+                  const newValue = inputRef.current?.value;
+                  if (newValue) {
+                    updateVariable(updateKey, newValue).then(() => {
+                      setTimeout(() => incrementEdits());
+                    });
+                  }
+                },
+              })}
+            />
+          </label>
+        </div>
+      )}
     </div>
   );
 };
